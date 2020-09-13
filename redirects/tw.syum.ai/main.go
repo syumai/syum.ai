ed467976c67df24a30d8ee1d6869d59add51c5a1
syumai <syumai@gmail.com>
2020-09-13 22:17:29 +0900

    Add redirect configs

---
+++ redirects/tw.syum.ai/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/syumai/syum.ai/redirects/redirect"
+)
+
+const location = "https://twitter.com/__syumai"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("Listening on port %s", port)
+
+	if err := redirect.Serve(location, port); err != nil {
+		log.Fatal(err)
+	}
+}
