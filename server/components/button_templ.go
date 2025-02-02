5580455a5dd840a40a1ba2796b97da876ee7b903
syumai <syumai@gmail.com>
2025-02-02 10:12:35 +0900

    add touch-manipulation to reload syumai button

--- server/components/button_templ.go
+++ server/components/button_templ.go
@@ -42,7 +42,7 @@ func Button(id string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" class=\"relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 cursor-pointer\"><span class=\"relative px-2.5 py-2 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-transparent\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\" class=\"relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 cursor-pointer touch-manipulation\"><span class=\"relative px-2.5 py-2 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-transparent\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
