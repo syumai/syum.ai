51c9ec19b188c8b495ffbbf2250ace06b04d0234
syumai <syumai@gmail.com>
2025-03-29 23:34:51 +0900

    add lang to html

--- server/pages/indexpage/index_templ.go
+++ server/pages/indexpage/index_templ.go
@@ -29,7 +29,7 @@ func Index(initialColorCode, colorCode string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<!doctype html><html lang=\"en\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
