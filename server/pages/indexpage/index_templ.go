2bfe88ed7ae2a3bd087f1d4540e6aa506bb1c331
syumai <syumai@gmail.com>
2025-03-30 01:00:37 +0900

    set main / footer

--- server/pages/indexpage/index_templ.go
+++ server/pages/indexpage/index_templ.go
@@ -37,7 +37,7 @@ func Index(initialColorCode, colorCode string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<body class=\"flex flex-col items-center space-y-5\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<body class=\"flex flex-col items-center gap-y-5\"><main class=\"flex flex-col items-center gap-y-5\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -53,7 +53,7 @@ func Index(initialColorCode, colorCode string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<a href=\"https://github.com/syumai/syum.ai\" class=\"text-blue-600 dark:text-blue-500 hover:underline\">github.com/syumai/syum.ai</a><script src=\"/js/index.js\" defer></script></body></html>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</main><footer><a href=\"https://github.com/syumai/syum.ai\" class=\"text-blue-600 dark:text-blue-500 hover:underline\">github.com/syumai/syum.ai</a></footer><script src=\"/js/index.js\" defer></script></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
