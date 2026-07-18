6581994782bd4ff22e7dc0685a828b60480a94ca
syumai <syumai@gmail.com>
2026-07-18 12:11:31 +0900

    allow cloudflare insights

--- server/handlers.go
+++ server/handlers.go
@@ -36,7 +36,7 @@ func NewHandler() http.Handler {
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'unsafe-inline'; img-src 'self' blob:;")
+	w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'unsafe-inline'; img-src 'self' blob:; script-src 'self' static.cloudflareinsights.com; connect-src 'self' cloudflareinsights.com;")
 	w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
 	w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 	colorCode := r.URL.Query().Get("colorCode")
